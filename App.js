e5fb68c3be6045e883f1b773f40bd2c5be3a61a2
thelegend2004 <oleh.liubichenko@gmail.com>
2025-10-02 01:10:49 +0300

    feature: add input dummy

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Aloha!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Enter your goal" />
+        <Button title="Click" />
+      </View>
+      <View>
+        <Text>Goals list</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#cad1f0',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    width: "80%",
+    marginRight: 8,
+    padding: 8,
   },
 });
---
+++ README.md
@@ -0,0 +1,3 @@
+# rnapp
+
+App to add and read you goals from the list on phone!
